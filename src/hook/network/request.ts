768ae1b9ac6a72362e0d52b1c8b2638906643a7c
DefectingCat <i@defect.ink>
2021-05-21 00:07:21 +0800

    首页基本布局 1. 添加菜单 2. 添加iconfont

--- src/hook/network/request.ts
+++ src/hook/network/request.ts
@@ -1,9 +1,17 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 const request = axios.create({
-  baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+  // baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+  baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
   timeout: 5000,
 });
+// 响应拦截器
 request.interceptors.response.use((value) => {
   return value.data;
+});
+// 请求拦截器 在 headers 中添加 token
+request.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem('token');
+  token ? (config.headers.Authorization = token) : void 0;
+  return config;
 });
 export default request;
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,108 @@
 <template>
-  <div><ElButton type="info" plain @click="logout">退出</ElButton></div>
+  <ElContainer>
+    <ElHeader>
+      <div class="head-left">
+        <svg class="icon head-icon" aria-hidden="true" @click="foldMenu">
+          <use xlink:href="#xfy-zhankai"></use>
+        </svg>
+        <span>后台管理系统</span>
+      </div>
+
+      <div class="head-right">
+        <ElButton type="info" plain @click="logout">退出</ElButton>
+      </div>
+    </ElHeader>
+
+    <ElContainer>
+      <ElAside>
+        <ElMenu
+          v-for="item of menus"
+          :key="item.id"
+          :collapse="isCollapse"
+          :unique-opened="true"
+        >
+          <ElSubmenu :index="item.id.toString()">
+            <template #title>
+              <svg class="icon" aria-hidden="true">
+                <use :xlink:href="icons[item.id]"></use>
+              </svg>
+              <span>{{ item.authName }}</span>
+            </template>
+
+            <ElMenuItemGroup>
+              <ElMenuItem
+                v-for="subItem of item.children"
+                :key="subItem.id"
+                :index="subItem.id.toString()"
+              >
+                <template #title>
+                  <i class="el-icon-menu"></i>
+                  {{ subItem.authName }}
+                </template>
+              </ElMenuItem>
+            </ElMenuItemGroup>
+          </ElSubmenu>
+        </ElMenu>
+      </ElAside>
+      <ElMain class="home-body">主体</ElMain>
+    </ElContainer>
+
+    <ElFooter>页脚</ElFooter>
+  </ElContainer>
 </template>
 
 <script lang="ts" setup>
-import { ElButton, ElMessage } from 'element-plus';
+import {
+  ElContainer,
+  ElHeader,
+  ElMain,
+  ElAside,
+  ElButton,
+  ElFooter,
+  ElMessage,
+  ElMenu,
+  ElSubmenu,
+  ElMenuItem,
+  ElMenuItemGroup,
+} from 'element-plus';
 import { useRouter } from 'vue-router';
+import request from '@/hook/network/request';
+import { reactive, toRefs } from 'vue';
+
+type State = {
+  menus: {
+    id: number;
+    authName: string;
+    path: string;
+    children: {
+      id: number;
+      authName: string;
+      path: string;
+      order: string;
+    }[];
+    order: string;
+  }[];
+  icons: Record<string, string>;
+  isCollapse: boolean;
+};
 
 const router = useRouter();
+const state = reactive<State>({
+  // 菜单列表
+  menus: [],
+  // id 对应图标
+  icons: {
+    '125': '#xfy-yonghu',
+    '103': '#xfy-quanxian',
+    '101': '#xfy-shangpin',
+    '102': '#xfy-dingdan',
+    '145': '#xfy-shuju',
+  },
+  isCollapse: false,
+});
+const { menus, icons, isCollapse } = toRefs(state);
 
+// 注销按钮
 const logout = () => {
   sessionStorage.clear();
   ElMessage({
@@ -17,6 +112,51 @@ const logout = () => {
   });
   router.push('login');
 };
+const getMenuList = async () => {
+  const { data: res } = await request({
+    method: 'GET',
+    url: 'menus',
+  });
+  menus.value = res;
+};
+getMenuList();
+
+const foldMenu = () => {
+  isCollapse.value = !isCollapse.value;
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-body {
+  height: 100%;
+  background-color: #edf1f7;
+}
+.el-header {
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .head-left {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    .head-icon {
+      cursor: pointer;
+      margin-right: 10px;
+    }
+  }
+  .head-right {
+    display: flex;
+  }
+}
+.el-aside {
+  background-color: #fff;
+  .icon {
+    width: 25px;
+    margin-right: 4px;
+  }
+}
+.el-container {
+  height: 100%;
+}
+</style>
