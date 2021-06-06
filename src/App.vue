82cedb54a376987c3f04a561747e6e326dccbb85
DefectingCat <i@defect.ink>
2021-06-06 17:21:17 +0800

    关闭缓存 1. 减少DOM节点

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <!-- <router-view v-slot="{ Component }">
     <keep-alive exclude="">
       <component :is="Component" />
     </keep-alive>
-  </router-view>
+  </router-view> -->
+  <router-view></router-view>
 </template>
 
 <style lang="scss"></style>
--- src/components/Home/Goods/GoodsTable.vue
+++ src/components/Home/Goods/GoodsTable.vue
@@ -15,23 +15,19 @@
     <ElTableColumn label="创建时间" prop="add_time" width="180"></ElTableColumn>
     <ElTableColumn label="操作" width="180">
       <template #default>
-        <ElTooltip effect="light" content="编辑商品" placement="top">
-          <el-button size="mini" icon="el-icon-edit"></el-button>
-        </ElTooltip>
-        <ElTooltip effect="light" content="删除商品" placement="top">
-          <el-button
-            size="mini"
-            icon="el-icon-delete"
-            type="danger"
-          ></el-button>
-        </ElTooltip>
+        <!-- <ElTooltip effect="light" content="编辑商品" placement="top"> -->
+        <el-button size="mini" icon="el-icon-edit"></el-button>
+        <!-- </ElTooltip> -->
+        <!-- <ElTooltip effect="light" content="删除商品" placement="top"> -->
+        <el-button size="mini" icon="el-icon-delete" type="danger"></el-button>
+        <!-- </ElTooltip> -->
       </template>
     </ElTableColumn>
   </ElTable>
 </template>
 
 <script lang="ts" setup>
-import { ElButton, ElTable, ElTableColumn, ElTooltip } from 'element-plus';
+import { ElButton, ElTable, ElTableColumn } from 'element-plus';
 import { defineProps } from 'vue-demi';
 import type { State } from '@/views/Home/Goods/GoodsLogic';
 
